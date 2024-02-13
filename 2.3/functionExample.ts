4315f8ed18917bee68673c7edd729a725957b65b
Marohn Howard <mars.howard@devmounta.in>
2024-02-12 19:59:38 -0700

    notes for 2.3

--- 2.3/functionExample.ts
+++ 2.3/functionExample.ts
@@ -3,7 +3,7 @@ function makeAMeal(order: string): string {
     console.log(order)
     return order + " $15.00"; 
 }; 
-
+// note
 let firstOrder = makeAMeal("Two Double Cheese Burgers, Onion Rings, and a Root Beer"); 
 let secondOrder = makeAMeal("Spicy Chicken Sandwich, Fries, and a Dr. Pepper"); 
 let thirdOrder = makeAMeal("10 Piece Chicken Nugget with BBQ Sauce, Fries, and a Pepsi"); 
